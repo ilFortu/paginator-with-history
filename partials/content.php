5c86d06bb3eca8fac3934e2076b115da7711e2f4
antoniofortunato <fortuu@gmail.com>
2016-02-25 18:28:27 +0100

    in progress

---
+++ partials/content.php
@@ -0,0 +1,33 @@
+<?php
+$page = $_GET["page"];
+
+
+for ($i = 0; $i < 9; $i++) {
+  ?>
+
+
+  <div id="item-<?= $page ?>-<?= $i ?>" class="item">
+
+    <h3> Titolo di prova <?= $page ?>-<?= $i ?></h3>
+
+
+    <div class="text">
+
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+      </p>
+
+
+    </div>
+
+    <a class="paginator-item-link" href="http://google.com">link</a>
+
+
+  </div>
+
+
+
+<?php
+}
+?>
--- test.php
+++ test.php
@@ -8,18 +8,12 @@
 
 
 <?php
+$pageCurrent = isset($_GET["page"]) ? $_GET["page"] : 1;
 
-
-$pageCurrent = $_GET["page"];
 $pagePrev = $pageCurrent - 1;
 $pageNext = $pageCurrent + 1;
 
-
-
-
 ?>
-
-
 
 <h1>Test</h1>
 
@@ -27,346 +21,51 @@ $pageNext = $pageCurrent + 1;
 <div class="container">
 
 
-  <div class="listing-container paginator-history" data-paginator-url="partials/content.html" data-base-url="test.html">
+  <div class="listing-container paginator-history" data-paginator-url="partials/content.php" data-base-url="test.php">
 
-    <a href="#" class="paginator prev" data-page="<?= $pagePrev ?>">previous page</a>
+    <?php //if ($pagePrev > 0) { ?>
 
+    <a href="test.php?page=<?= $pagePrev ?>" class="paginator prev" data-page="<?= $pagePrev ?>">previous page</a>
+
+    <? //} ?>
 
     <div class="listing-content paginator-list-container">
 
 
       <div class="list-items-page" data-page="<?= $pageCurrent ?>">
-        <div id="item-1" class="item">
-
-          <h3>Titolo di prova 1</h3>
 
 
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
+        <?php
+        for ($i = 0; $i < 9; $i++) {
+          ?>
 
 
+          <div id="item-<?= $pageCurrent ?>-<?= $i ?>" class="item">
+
+            <h3>Titolo di prova <?= $pageCurrent ?>-<?= $i ?></h3>
+
+
+            <div class="text">
+
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's
+                standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+              </p>
+            </div>
+
+            <a class="paginator-item-link" href="http://google.com">link</a>
           </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-        <div id="item-2" class="item">
-
-          <h3> Titolo di prova 2</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-        <div id="item-3" class="item">
-
-          <h3> Titolo di prova 3</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-        <div id="item-4" class="item">
-
-          <h3> Titolo di prova 4</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-
-        <div id="item-5" class="item">
-
-          <h3> Titolo di prova 5</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-
-        <div id="item-6" class="item">
-
-          <h3> Titolo di prova 6</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-
-        <div id="item-7" class="item">
-
-          <h3> Titolo di prova 7</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-        <div id="item-8" class="item">
-
-          <h3> Titolo di prova 8</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-        <div id="item-9" class="item">
-
-          <h3> Titolo di prova 9</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-        <div id="item-10" class="item">
-
-          <h3> Titolo di prova 10</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-        <div id="item-11" class="item">
-
-          <h3> Titolo di prova 11</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-
-        <div id="item-12" class="item">
-
-          <h3> Titolo di prova 12</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-
-        <div id="item-13" class="item">
-
-          <h3> Titolo di prova 13</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-
-        <div id="item-14" class="item">
-
-          <h3> Titolo di prova 14</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-        <div id="item-15" class="item">
-
-          <h3> Titolo di prova 15</h3>
-
-
-          <div class="text">
-
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            </p>
-
-
-          </div>
-
-          <a class="paginator-item-link" href="http://google.it">link</a>
-
-
-        </div>
-
-
+        <?php
+        }
+        ?>
       </div>
     </div>
 
-    <a href="test.html?page=2" class="paginator next" data-page="<?= $pageNext ?>">next page</a>
-
+    <a href="test.php?page=<?= $pageNext ?>" class="paginator next" data-page="<?= $pageNext ?>">next page</a>
   </div>
 
 </div>
-
-
 <script src="docroot/js/jquery">/**/</script>
 <script src="docroot/js/jquery.paginator.history.js">/**/</script>
 <script src="docroot/js/main.js">/**/</script>
